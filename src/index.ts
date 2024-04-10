bda46a1b1b1b5d85673b3dd1300151a85ea7820a
jameskehs <jameskehsdev@gmail.com>
2024-04-10 10:32:21 -0500

    Test Index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hi');
+alert('TEST');
+
+async function fetchScriptSrcFromRepo(gitUsername, repoName, fileName) {
+  const apiUrl = `https://api.github.com/repos/${username}/${repoName}/commits/main`;
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return `https://cdn.jsdelivr.net/gh/${gitUsername}/${repoName}@${data.sha}/build/${fileName}`;
+  } catch (error) {
+    console.error('Failed to fetch the latest commit hash:', error);
+  }
+}
+
+fetchScriptSrcFromRepo('jameskehs', 'VividStorefronts', 'index.js').then((scriptSrc) => {
+  if (scriptSrc) {
+    $.getScript(scriptSrc);
+  }
+});
