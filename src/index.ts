0e7bb7faacc7c0fdaf2f20c4e6e870005606450a
James Kehs <jameskehsdev@gmail.com>
2024-05-20 22:16:09 -0500

    Remove loader

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ export const GLOBALVARS: GlobalState = {
 
 async function loadStorefrontScript(groupID: number) {
   try {
-    startLoading();
     //~~~~~ Set Global Variables ~~~~~//
     GLOBALVARS.currentPage = Utils.determineCurrentPage();
 
@@ -18,7 +17,6 @@ async function loadStorefrontScript(groupID: number) {
     let scriptFolder = ScriptMap[groupID];
     if (scriptFolder === undefined) {
       throw new Error(`Module with groupID ${groupID} not found in ModuleMap.`);
-      return;
     }
 
     //~~~~~ Load our scripts and styles ~~~~~//
@@ -37,22 +35,7 @@ async function loadStorefrontScript(groupID: number) {
     }
   } catch (error: unknown) {
     console.error(error);
-  } finally {
-    endLoading();
   }
-}
-
-function startLoading() {
-  $('body').append(`<div id="external-script-loader"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>`);
-  console.log('LOADING');
-}
-
-function endLoading() {
-  $('#external-script-loader').addClass('inactive');
-  setTimeout(() => {
-    $('#external-script-loader').remove();
-  }, 500);
-  console.log('FINISHED LOADING');
 }
 
 //~~~~~ Expose loadStorefrontScript to the window ~~~~~//
