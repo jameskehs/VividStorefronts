755954659662988a3f237af44f9cd1edb821a972
James Kehs <jameskehsdev@gmail.com>
2024-05-20 21:37:42 -0500

    More styling tweaks

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,7 +2,7 @@ export function runBaseScript() {
   console.log('Hello from shared script!');
 
   //Wrap nav elements
-  $('.tableSiteBanner, #navWrapper').wrapAll(`<div class="logoLinks"></div>`);
-  $('.logoLinks').wrapAll(`<div class="navWrapper"></div>`);
-  $('.tableLogin').wrapAll("<div class='loginWrapper'></div>");
+  $('.tableSiteBanner, #navWrapper').wrapAll(`<div id="logoLinks"></div>`);
+  $('.logoLinks').wrapAll(`<div id="navWrapper"></div>`);
+  $('.tableLogin').wrapAll("<div id='loginWrapper'></div>");
 }
