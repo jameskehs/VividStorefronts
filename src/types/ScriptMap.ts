bf9dfec7b0b4728212dce82eccda120d0171ba8f
jameskehs <jameskehsdev@gmail.com>
2024-05-24 09:44:46 -0500

    Update PB ScriptMap

--- src/types/ScriptMap.ts
+++ src/types/ScriptMap.ts
@@ -2,7 +2,7 @@ export const ScriptMap: Record<number, string> = {
   58: 'RaisingCanes',
   66: 'JKTest',
   111: 'TheGivingKitchen',
-  123: 'PenningtonBiomedical',
   124: 'EpicPiping',
   127: 'DPiAnesthesia',
+  130: 'PenningtonBiomedical',
 };
