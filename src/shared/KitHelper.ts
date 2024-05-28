fa93357caf106a2ca9a03e142f37e4823f92bba9
jameskehs <jameskehsdev@gmail.com>
2024-05-28 14:03:25 -0500

    Kit Fixes

--- src/shared/KitHelper.ts
+++ src/shared/KitHelper.ts
@@ -203,9 +203,7 @@ export class KitWorkflow {
 
       if (this.activeKit.enforceRecommendedQty) {
         $('input#quantity').val(this.activeKit.items[this.activeKit.index].recommendedQty).prop('disabled', true);
-        setTimeout(() => {
-          $("'input#quantity'").trigger('change');
-        }, 1000);
+        $('input#quantity').trigger('change');
       }
     }
   }
