27e97b4c8dd650ccbf5de073ac40fd148822a695
jameskehs <jameskehsdev@gmail.com>
2024-06-07 15:55:39 -0500

    Remove On Demand Tag

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -13,10 +13,10 @@ export function runSharedScript(options: OptionsParameter) {
   options.hideCompanyShipTo && $('div#shipToCompany').hide();
 
   // Add "On Demand" tag to products that are not inventoried
-  $('.prodCell').each(function (index, cell) {
+  /*$('.prodCell').each(function (index, cell) {
     const inventoryTag = $(this).find('.meta p.ui-state-error, .meta p.ui-state-default');
     if (inventoryTag.length === 0) {
       $('span.meta', this).prepend('<p class="ui-state-default ui-corner-all" style="text-align: center;">On Demand</p>');
     }
-  });
+  });*/
 }
