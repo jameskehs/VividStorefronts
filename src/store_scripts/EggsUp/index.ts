0d6a034d15f0c1246fefdec34af1dacafaeb8966
joshmilton <joshmilton@vividink.com>
2024-10-28 14:18:06 -0500

    Payment Disclaimer

--- src/store_scripts/EggsUp/index.ts
+++ src/store_scripts/EggsUp/index.ts
@@ -38,7 +38,7 @@ export function main() {
   }
 }
 if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTPAYMENT) {
-  $("#CCInstructions").val(
-    "Your credit card will be authorized once the order is placed. This may put a hold on funds with your banking institution. Please note that we do not charge your credit card until the order has shipped. Most times the shipping cost is less than estimated shipping cost shown."
+  $("#CCInstructions").text(
+    "Your credit card will be authorized once the order is placed. This may put a hold on funds with your banking institution. Please note that we do not charge your credit card until the order has shipped. Most times the shipping cost is less than estimated shipping cost shown.><br>Enter the information below to complete the payment options for your order."
   );
 }
