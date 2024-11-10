826bee551660bd3ddba24272b18d4407ce449b6d
jameskehs <jameskehsdev@gmail.com>
2024-11-10 16:08:07 -0600

    Set image picker selection in memo field

---
+++ src/shared/imagePickertoMemo.ts
@@ -0,0 +1,21 @@
+import { StorefrontPage } from '../enums/StorefrontPage.enum';
+import { GLOBALVARS } from '../index';
+
+export function AddImagePickerSelectionToMemo() {
+  if (GLOBALVARS.currentPage === StorefrontPage.CUSTOMIZETEMPLATE) {
+    const selectedImage = $('.image-picker-ui-name').text();
+    console.log({ selectedImage });
+    if (selectedImage) {
+      localStorage.setItem('imagePickerSelection', selectedImage);
+    }
+  }
+
+  if (GLOBALVARS.currentPage === StorefrontPage.CART) {
+    const selectedImage = localStorage.getItem('imagePickerSelection');
+    console.log({ selectedImage });
+    if (!selectedImage) return;
+
+    const newMemoFieldValue = selectedImage + $('.memoRow input').last().val()?.toString().replace('Your job name/memo here', '');
+    $('#shoppingCartTbl .memoRow input').last().val(newMemoFieldValue).trigger('blur');
+  }
+}
