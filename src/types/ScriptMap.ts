73ddd78bf2a2ac01f3e2eb6c41fa787fc3442ad5
joshmilton <joshmilton@vividink.com>
2025-03-10 13:01:48 -0500

    Adding Don's

--- src/types/ScriptMap.ts
+++ src/types/ScriptMap.ts
@@ -2,6 +2,7 @@ export const ScriptMap: Record<number, string> = {
   10: "McAlisters",
   58: "RaisingCanes",
   55: "Little-Greek",
+  56: "DonsSeafood",
   66: "JKTest",
   67: "ShipleyDonuts",
   75: "ShipleyB2C",
