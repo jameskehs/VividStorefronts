e9b86424ad888f962bca1854aff850c473786d45
joshmilton <joshmilton@vividink.com>
2025-04-09 13:44:22 -0500

    Coastal

--- src/shared/checkoutBody.ts
+++ src/shared/checkoutBody.ts
@@ -2,21 +2,19 @@ import { StorefrontPage } from "../enums/StorefrontPage.enum";
 import { GLOBALVARS } from "../index";
 
 export function ChangeCheckoutBody(newMessage: string, email: string) {
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTADDRESS) {
-    // Get the element with the id 'checkoutBody'
-    var element = document.getElementById("checkoutBody");
+  // Get the element with the id 'checkoutBody'
+  var element = document.getElementById("checkoutBody");
 
-    // Create the mailto link for the provided email address
-    var emailLink = `<a href="mailto:${email}">${email}</a>`;
+  // Create the mailto link for the provided email address
+  var emailLink = `<a href="mailto:${email}">${email}</a>`;
 
-    // Replace the email address in the message with the mailto link
-    var updatedMessage = newMessage.replace("angela@vividink.com", emailLink);
+  // Replace the email address in the message with the mailto link
+  var updatedMessage = newMessage.replace("angela@vividink.com", emailLink);
 
-    // Set the updated message to the innerHTML of the element
-    if (element) {
-      element.innerHTML = updatedMessage;
-    } else {
-      console.log("Element with id 'checkoutBody' not found.");
-    }
+  // Set the updated message to the innerHTML of the element
+  if (element) {
+    element.innerHTML = updatedMessage;
+  } else {
+    console.log("Element with id 'checkoutBody' not found.");
   }
 }
