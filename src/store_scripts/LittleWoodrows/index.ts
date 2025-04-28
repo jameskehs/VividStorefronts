fe8b7483e5f3db12a0b9c161734cf823db96ceae
joshmilton <joshmilton@vividink.com>
2025-04-28 14:32:56 -0500

    Little Woodrows Inventory Meesage

--- src/store_scripts/LittleWoodrows/index.ts
+++ src/store_scripts/LittleWoodrows/index.ts
@@ -3,6 +3,7 @@
 
 import { StorefrontPage } from "../../enums/StorefrontPage.enum";
 import { GLOBALVARS } from "../../index";
+import { ChangeInventoryCountNotice } from "../../shared/inventoryCountNotice";
 
 export function main() {
   console.log(GLOBALVARS.currentPage);
@@ -32,3 +33,8 @@ export function main() {
   if (GLOBALVARS.currentPage === StorefrontPage.VIEWORDERS) {
   }
 }
+// Example call with a dynamic email
+ChangeInventoryCountNotice(
+  "Inventory not available for the desired order quantity. Please contact your account manager at 225-751-7297, or by email at angela@vividink.com",
+  "racheleast@vividink.com" // Dynamically changing the email address
+);
