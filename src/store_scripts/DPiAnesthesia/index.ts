a35e5f68ec261cf6286ca9a3b5518bd59635143e
joshmilton <joshmilton@vividink.com>
2025-05-15 09:13:12 -0500

    dpi index cleanup

--- src/store_scripts/DPiAnesthesia/index.ts
+++ src/store_scripts/DPiAnesthesia/index.ts
@@ -5,29 +5,22 @@ import { setupCustomerPORequirement } from "../../shared/requireCustomerPO";
 export function main() {
   console.log(GLOBALVARS.currentPage);
 
-  if (GLOBALVARS.currentPage === StorefrontPage.ADDTOCART) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CART) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CATALOG) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTADDRESS) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTCONFIRMATION) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTPAYMENT) {
-    setupCustomerPORequirement();
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTREVIEW) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTSHIPPING) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CREATEEDITACCOUNT) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.CUSTOMIZETEMPLATE) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.MYACCOUNT) {
-  }
-  if (GLOBALVARS.currentPage === StorefrontPage.VIEWORDERS) {
+  switch (GLOBALVARS.currentPage) {
+    case StorefrontPage.ADDTOCART:
+    case StorefrontPage.CART:
+    case StorefrontPage.CATALOG:
+    case StorefrontPage.CHECKOUTADDRESS:
+    case StorefrontPage.CHECKOUTCONFIRMATION:
+    case StorefrontPage.CHECKOUTREVIEW:
+    case StorefrontPage.CHECKOUTSHIPPING:
+    case StorefrontPage.CREATEEDITACCOUNT:
+    case StorefrontPage.CUSTOMIZETEMPLATE:
+    case StorefrontPage.MYACCOUNT:
+    case StorefrontPage.VIEWORDERS:
+      break;
+
+    case StorefrontPage.CHECKOUTPAYMENT:
+      setupCustomerPORequirement(); // 🟢 This already handles the form
+      break;
   }
 }
