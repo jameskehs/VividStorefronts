bd390bd5e4b797638ecfc7b6e83cc1a28ed8951c
joshmilton <joshmilton@vividink.com>
2025-06-09 16:34:37 -0500

    Salata

--- src/store_scripts/Salata/index.ts
+++ src/store_scripts/Salata/index.ts
@@ -4,7 +4,6 @@
 import { StorefrontPage } from "../../enums/StorefrontPage.enum";
 import { GLOBALVARS } from "../../index";
 import { replaceAttrText } from "../../shared/replaceSizeText";
-import { ChangeInventoryCountNotice } from "../../shared/inventoryCountNotice";
 
 export function main() {
   console.log(GLOBALVARS.currentPage);
@@ -45,8 +44,3 @@ export function main() {
   if (GLOBALVARS.currentPage === StorefrontPage.VIEWORDERS) {
   }
 }
-// Example call with a dynamic email
-ChangeInventoryCountNotice(
-  "Inventory not available for the desired order quantity. Please contact your account manager at 225-751-7297, or by email at angela@vividink.com",
-  "kfreeman@poweredbyprisma.com" // Dynamically changing the email address
-);
