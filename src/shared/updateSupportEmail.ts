c32e13a1227d49199945896a2601676afedd3038
joshmilton <joshmilton@vividink.com>
2025-06-12 09:50:54 -0500

    same

--- src/shared/updateSupportEmail.ts
+++ src/shared/updateSupportEmail.ts
@@ -1,11 +1,18 @@
+import { StorefrontPage } from "../enums/StorefrontPage.enum";
+import { GLOBALVARS } from "../index";
+
 export function updateSupportEmail(newEmail: string): void {
-  const emailLink = document.querySelector(
-    'a[href^="mailto:artdepartment@poweredbyprisma.com"]'
-  ) as HTMLAnchorElement;
-  if (emailLink) {
-    emailLink.href = `mailto:${newEmail}`;
-    emailLink.textContent = newEmail;
-  } else {
-    console.warn("Email link not found.");
+  if (GLOBALVARS.currentPage === StorefrontPage.CREATEEDITACCOUNT) {
+    // Select the <a> tag inside the checkoutBody td
+    const emailLink = document.querySelector(
+      "td.checkoutBody a[href^='mailto:']"
+    ) as HTMLAnchorElement;
+
+    if (emailLink) {
+      emailLink.href = `mailto:${newEmail}`;
+      emailLink.textContent = newEmail;
+    } else {
+      console.warn("Support email link not found");
+    }
   }
 }
