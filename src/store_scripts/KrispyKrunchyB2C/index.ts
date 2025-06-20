5c9e62f1a4adbae5e9ac340e045907bf7bf26c89
joshmilton <joshmilton@vividink.com>
2025-06-20 08:39:46 -0500

    Krispy Redirect

--- src/store_scripts/KrispyKrunchyB2C/index.ts
+++ src/store_scripts/KrispyKrunchyB2C/index.ts
@@ -1,35 +1,35 @@
 // <script src="https://main--vividstorefronts.netlify.app/main.js"></script>
 // <script>loadStorefrontScript(brandingProfile)</script>
 
-import { StorefrontPage } from '../../enums/StorefrontPage.enum';
-import { GLOBALVARS } from '../../index';
+import { StorefrontPage } from "../../enums/StorefrontPage.enum";
+import { GLOBALVARS } from "../../index";
 
 export function main() {
   console.log(GLOBALVARS.currentPage);
 
-  document.title = 'Krispy Krunchy Online Store';
+  document.title = "Krispy Krunchy Online Store";
 
-  let shipMsg = `<p class="ship-message">Orders over $100 receive free shipping.<br>Please NOTE: Shipping will show in your total at the checkout process but your card will only be charged for items ordered - no shipping!</p>`;
-  $(shipMsg).insertBefore('.tableMain');
+  let shipMsg = `<p class="ship-message">We've moved!<br>Click<a href="https://www.krispykrunchy.com/krunch-shop/">here</a> to access our brand-new online store for the latest products and a smoother shopping experience. We can't wait to see you there!</p>`;
+  $(shipMsg).insertBefore(".tableMain");
 
   // Change "Shipping & Handling" to "Shipping" in the order summary
-  $('#taxRushShipGrand table tbody tr td').eq(7).text('Shipping');
+  $("#taxRushShipGrand table tbody tr td").eq(7).text("Shipping");
 
   // Remove option to use default customer billing address
-  $('#billAdrOnFileBox').remove();
+  $("#billAdrOnFileBox").remove();
 
   // Remove the Edit Profile Form
-  $('#editProfileTbl tbody').eq(1).remove();
+  $("#editProfileTbl tbody").eq(1).remove();
 
   if (GLOBALVARS.currentPage === StorefrontPage.ADDTOCART) {
     // Hide add to cart button when editing an item that was already added to the cart
-    if ($('#returnToCartButton').css('display') === 'block') {
-      $('#addToCartButton').remove();
+    if ($("#returnToCartButton").css("display") === "block") {
+      $("#addToCartButton").remove();
     }
 
     // Hide the qtyAvailable row when adding an item to cart
-    $('.tablesorter tr').each(function (index, element) {
-      if ($(this).html().includes('qtyAvailable')) {
+    $(".tablesorter tr").each(function (index, element) {
+      if ($(this).html().includes("qtyAvailable")) {
         $(this).hide();
       }
     });
