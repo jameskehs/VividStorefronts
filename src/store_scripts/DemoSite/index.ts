796c1f6f83b440ca1f31e142299ed443634d2dcd
joshmilton <joshmilton@vividink.com>
2025-07-08 16:15:55 -0500

    same

--- src/store_scripts/DemoSite/index.ts
+++ src/store_scripts/DemoSite/index.ts
@@ -3,7 +3,11 @@ import { GLOBALVARS } from "../../index";
 import { applyPromoCode } from "../../shared/ApplyPromoCode";
 import { persistDiscountedTotals } from "../../shared/persistDiscountedTotals";
 
-console.log("[DEBUG] GLOBALVARS.currentPage =", GLOBALVARS.currentPage);
+console.log("[EARLY] GLOBALVARS.currentPage =", GLOBALVARS.currentPage);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("[LATE] GLOBALVARS.currentPage =", GLOBALVARS.currentPage);
+});
 
 export function main() {
   console.log(GLOBALVARS.currentPage);
