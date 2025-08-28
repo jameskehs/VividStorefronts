9eae0af1cced734e927cd4bb6e1eaf47aebde932
joshmilton <joshmilton@vividink.com>
2025-08-28 15:35:02 -0500

    Pennington Index Update

--- src/store_scripts/PenningtonBiomedical/index.ts
+++ src/store_scripts/PenningtonBiomedical/index.ts
@@ -1,16 +1,38 @@
 //<script src="https://main--vividstorefronts.netlify.app/dist/main.js"></script>
 //<script>loadStorefrontScript(brandingProfile)</script>
 
-import { StorefrontPage } from '../../enums/StorefrontPage.enum';
-import { GLOBALVARS } from '../../index';
+import { StorefrontPage } from "../../enums/StorefrontPage.enum";
+import { GLOBALVARS } from "../../index";
 
 export function main() {
   console.log(GLOBALVARS.currentPage);
 
+  if (GLOBALVARS.currentPage === StorefrontPage.ADDTOCART) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CART) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CATALOG) {
+  }
   if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTADDRESS) {
-    $('#shipInstructions').css('display', 'none');
-    $('.tableMain').prepend(
+    $("#shipInstructions").css("display", "none");
+    $(".tableMain").prepend(
       '<h2 style="font-size:16px;border:2px solid #3e1c84;padding:12px;text-align:center;font-weight:bold">Personal orders should not be shipped to Pennington Biomedical.</h2>'
     );
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTCONFIRMATION) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTPAYMENT) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTREVIEW) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CHECKOUTSHIPPING) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CREATEEDITACCOUNT) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.CUSTOMIZETEMPLATE) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.MYACCOUNT) {
+  }
+  if (GLOBALVARS.currentPage === StorefrontPage.VIEWORDERS) {
   }
 }
